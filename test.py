37452083099e29e6cbbc7a88e67c806ed157c4e8
Reddy14731 <109854059+Reddy14731@users.noreply.github.com>
2022-07-24 22:44:10 +0530

    Added test cases

--- test.py
+++ test.py
@@ -5,7 +5,15 @@ import unittest
 class TestArithmetic(unittest.TestCase):
 
     def test_addition(self):
-        self.assertEqual(main.add(1, 2), 4, msg='Equal')
+        self.assertEqual(main.add(1, 2), 3, msg='Equal')
+    def test_sub(self):
+        self.assertEqual(main.sub(2, 2), 0, msg='Equal')
+    def test_sub(self):
+        self.assertEqual(main.sub(4, 2), 2, msg='Equal')
+    def test_sub(self):
+        self.assertEqual(main.mul(4, 2), 8, msg='Equal')
+    def test_div(self):
+        self.assertEqual(main.mul(4, 2), 2, msg='Equal')
 
 
 if __name__ == '__main__':
